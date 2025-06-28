e2705e9bbe3ff5726333917dce99e4ad42e5c9f4
Xyndra <71836523+Xyndra@users.noreply.github.com>
2025-06-28 20:11:13 +0200

    Initial Commit

---
+++ fabric/build.gradle.kts
@@ -0,0 +1,94 @@
+plugins {
+	id("fabric-loom")
+	id("maven-publish")
+	kotlin("jvm")
+}
+
+val minecraftVersion: String by project
+val loaderVersion: String by project
+val fabricVersion: String by project
+val fabricKotlinVersion: String by project
+
+val modVersion: String by project
+val mavenGroup: String by project
+val modId: String by project
+
+base {
+	archivesName = modId
+}
+
+repositories {
+	// Add repositories to retrieve artifacts from in here.
+	// You should only use this when depending on other mods because
+	// Loom adds the essential maven repositories to download Minecraft and libraries from automatically.
+	// See https://docs.gradle.org/current/userguide/declaring_repositories.html
+	// for more information about repositories.
+}
+
+fabricApi {
+	configureDataGeneration {
+		client = true
+	}
+}
+
+dependencies {
+	// To change the versions see the gradle.properties file
+	minecraft("com.mojang:minecraft:${minecraftVersion}")
+	mappings(loom.officialMojangMappings())
+	modImplementation("net.fabricmc:fabric-loader:${loaderVersion}")
+
+	// Fabric API. This is technically optional, but you probably want it anyway.
+	modImplementation("net.fabricmc.fabric-api:fabric-api:${fabricVersion}")
+	modImplementation("net.fabricmc:fabric-language-kotlin:${fabricKotlinVersion}")
+}
+
+tasks.named<ProcessResources>("processResources") {
+	filesMatching("fabric.mod.json") {
+		expand("version" to modVersion)
+	}
+}
+
+tasks.withType<JavaCompile>().configureEach {
+	options.release.set(21)
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+	compilerOptions {
+		jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+	}
+}
+
+
+java {
+	// Loom will automatically attach sourcesJar to a RemapSourcesJar task and to the "build" task
+	// if it is present.
+	// If you remove this line, sources will not be generated.
+	withSourcesJar()
+
+	sourceCompatibility = JavaVersion.VERSION_21
+	targetCompatibility = JavaVersion.VERSION_21
+}
+
+tasks.named<Jar>("jar") {
+	from("LICENSE") {
+		rename { "${it}_${modId}" }
+	}
+}
+
+// configure the maven publication
+publishing {
+	publications {
+		create<MavenPublication>("mavenJava") {
+			artifactId = modId
+			from(components["java"])
+		}
+	}
+
+	// See https://docs.gradle.org/current/userguide/publishing_maven.html for information on how to set up publishing.
+	repositories {
+		// Add repositories to publish to here.
+		// Notice: This block does NOT have the same function as the block in the top level.
+		// The repositories here will be used for publishing your artifact, not for
+		// retrieving dependencies.
+	}
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven {
+			name = "NeoForged Maven"
+			url = uri("https://maven.neoforged.net/releases")
+		}
+		maven {
+			name = "Fabric"
+			url = uri("https://maven.fabricmc.net/")
+		}
+		mavenCentral()
+	}
+	val loomVersion: String by settings
+	plugins {
+		id("fabric-loom") version loomVersion apply false
+	}
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
+
+val modName: String by settings
+rootProject.name = modName
+include(":fabric", ":neo", ":common")
