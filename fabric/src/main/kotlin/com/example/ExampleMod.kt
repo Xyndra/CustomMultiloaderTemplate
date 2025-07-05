d792c9c4d1364ab6ef1fe315e57d961b4879521e
Xyndra <71836523+Xyndra@users.noreply.github.com>
2025-07-05 14:00:29 +0200

    Also do dynamic registries on fabric
    Took 1 hour 3 minutes

--- fabric/src/main/kotlin/com/example/ExampleMod.kt
+++ fabric/src/main/kotlin/com/example/ExampleMod.kt
@@ -1,6 +1,12 @@
 package com.example
 
 import net.fabricmc.api.ModInitializer
+import net.minecraft.core.Registry
+import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.core.registries.Registries
+import net.minecraft.resources.ResourceKey
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.item.Item
 import org.slf4j.LoggerFactory
 
 object ExampleMod : ModInitializer {
@@ -11,6 +17,12 @@ object ExampleMod : ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 		logger.info("Hello Fabric world!")
+
 		Globals.loadAssets()
+		for ((name, constructor) in Globals.items) {
+			Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(ProjectProps["modId"], name),
+				constructor(Item.Properties().setId(
+					ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(ProjectProps["modId"], name)))))
+		}
 	}
 }
